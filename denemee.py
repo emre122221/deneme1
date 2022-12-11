be7aec0c625c6ae4ec515f1a9c5eec5796f95f47
emre122221 <emredalgic023@gmail.com>
2022-12-11 20:11:31 +0300

    gituba deneme proje gönderme

---
+++ denemee.py
@@ -0,0 +1,15 @@
+urunler = { 
+    100 :{
+        "urunAdi" : "Telefon",
+        "urunAdedi" : 80,
+        "garantiSuresi" : 2}
+        
+     101 : {
+       "urunAdi" : "kulaklık",
+       "urunAdedi" : 200,
+       "garantiSuresi" : 2}
+}
+
+sonuc = urunler[100]["urunAdi"]
+print(urunler)
+print(sonuc)
